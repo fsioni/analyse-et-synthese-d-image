514f1b57a3ed70f8218973224040b4e52a0535b1
LeXillal <julien38.b@gmail.com>
2023-04-12 17:05:21 +0200

    A la date :)

--- analyse/main.py
+++ analyse/main.py
@@ -3,7 +3,6 @@ import time
 import cv2
 import numpy as np
 
-
 # Récupérer la couleur dominante dans une image
 def couleur_dominante(_image):
     print('Récupération de la couleur dominante...')
@@ -26,25 +25,21 @@ def couleur_dominante(_image):
 
 
 # récupérer la liste des pixels de couleur dominante dans une image avec une tolérance
-def liste_pixel_dominants(_image, couleur, tolerance, gris=False):
+def liste_pixel_dominants(image, couleur, tolerance):
     print('Récupération de la liste des pixels de couleur dominante...')
-    _liste = []
-    for i in range(_image.shape[0]):
-        for j in range(_image.shape[1]):
-            pixel_color = _image[i][j]
-            limite_base = couleur - tolerance
-            limite_haut = couleur + tolerance
-            cond_haut = pixel_color[0] <= limite_haut[0] and pixel_color[1] <= limite_haut[1] and pixel_color[2] <= \
-                        limite_haut[2]
-            cond_base = pixel_color[0] >= limite_base[0] and pixel_color[1] >= limite_base[1] and pixel_color[2] >= \
-                        limite_base[2]
-            if cond_haut and cond_base:
-                _liste.append([i, j])
-    return _liste
+
+    limite_base = couleur - tolerance
+    limite_haut = couleur + tolerance
+
+    masque = np.all((image >= limite_base) & (image <= limite_haut), axis=-1)
+
+    indices = np.column_stack(np.nonzero(masque))
+
+    return indices
 
 
 # remplacer si dans la liste par une couleur sinon par une autre
-def remplacer_couleur(_image, _liste):
+def remplacer_couleur(_image, _liste, border=False):
     print('Image noir et blanc...')
     # Si dans la liste, on met en blanc
     # Sinon on le met en noir
@@ -54,11 +49,12 @@ def remplacer_couleur(_image, _liste):
         _image[pixel[0]][pixel[1]] = [255, 255, 255]
 
     # On hardcode le contour en blanc de x pixels de large
-    x = 6
-    _image[0:x, 0:_image.shape[1]] = [255, 255, 255]
-    _image[_image.shape[0] - x:_image.shape[0], 0:_image.shape[1]] = [255, 255, 255]
-    _image[0:_image.shape[0], 0:x] = [255, 255, 255]
-    _image[0:_image.shape[0], _image.shape[1] - x:_image.shape[1]] = [255, 255, 255]
+    if border:
+        x = border
+        _image[0:x, 0:_image.shape[1]] = [255, 255, 255]
+        _image[_image.shape[0] - x:_image.shape[0], 0:_image.shape[1]] = [255, 255, 255]
+        _image[0:_image.shape[0], 0:x] = [255, 255, 255]
+        _image[0:_image.shape[0], _image.shape[1] - x:_image.shape[1]] = [255, 255, 255]
 
     print('Image créée')
     return _image
@@ -77,7 +73,72 @@ def identification_amas(_image):
         # On vérifie que le contour fait au moins 100 pixels de large
         if w > 100:
             contours_amas.append(contour)
+
+
+    # on supprime le plus grand contour (le contour de l'image)
+    contours_amas.pop(0)
     return contours_amas
+
+def encadrer_amas(_image, _contours):
+    print('Encadrement des amas de pixels noirs...')
+    # on copie l'image de base
+    _image = _image.copy()
+    # On encadre les amas de pixels noirs
+    for contour in _contours:
+        # On récupère les coordonnées du contour
+        x, y, w, h = cv2.boundingRect(contour)
+        # On encadre le contour
+        cv2.rectangle(_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    return _image
+
+def numeroter_amas(_image, _contours):
+    print('Numérotation des amas de pixels noirs...')
+    # On encadre les amas de pixels noirs
+    for i, contour in enumerate(_contours):
+        # On récupère les coordonnées du contour
+        x, y, w, h = cv2.boundingRect(contour)
+        # On écrit le text en plein milieu du contour
+        cv2.putText(_image, str(i + 1), (x + int(w / 2), y + int(h / 2)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    return _image
+
+# on fait une image qui contient une seul piece (amas de pixels noirs)
+def piece_info(_contours, _index, image):
+    # On récupère les coordonnées du contour
+    x, y, w, h = cv2.boundingRect(_contours[_index])
+    # On affiche le contour
+    image = image.copy()
+    image = cv2.drawContours(image, _contours, _index, (0, 255, 0), 2)
+    # On compte le nombre de fois on traverse les verts sur le bord supérieur
+    count = 0
+    inGreen = False
+    greenPos = []
+    ptsUn = int(w/3)
+    ptsDeux = int(w/3*2)
+    for i in range(ptsUn, ptsDeux):
+        vert = image[y + 10][x + i][1] == 255 and image[y + 10][x + i][0] == 0 and image[y + 10][x + i][2] == 0
+        if vert:
+            inGreen = True
+            greenPos.append(i)
+            if (inGreen) and (image[y + 10][x + i + 1][1] != 255):
+                count += 1
+                inGreen = False
+                # On fait un point de rayon 5 au centre des greenPos
+                # On trouve le point du milieu
+                if len(greenPos) > 1:
+                    greenPos = int((greenPos[0] + greenPos[-1]) / 2)
+                else:
+                    greenPos = greenPos[0]
+
+                # On fait un point de rayon 5 au centre des greenPos
+                cv2.circle(image, (x + greenPos, y + 10), 3, (0, 0, 255), -1)
+                greenPos = []
+
+    print('Nombre de fois on traverse verts sur le bord supérieur :', count)
+
+
+    # On affiche juste l'image pour visualiser
+    imagePiece = image[y:y + h, x:x + w]
+    return imagePiece
 
 
 if __name__ == '__main__':
@@ -87,8 +148,8 @@ if __name__ == '__main__':
     # Open image.jpeg
     imageBase = cv2.imread('image.jpeg')
     imageGray = cv2.cvtColor(imageBase, cv2.COLOR_BGR2GRAY)
-    # Flou gaussien pour lisser l'image
-    imageBase = cv2.GaussianBlur(imageBase, (5, 5), 0)
+    imageBase = cv2.GaussianBlur(imageBase, (5, 5), 0) # blur pour réduire le bruit
+    # Couleur dominante
     dominant = couleur_dominante(imageBase)
     # Afficher la couleur de fond
     print('La couleur du fond est :', dominant)
@@ -98,36 +159,31 @@ if __name__ == '__main__':
     print('Nombre de pixels de couleur de fond :', len(liste))
 
     # Remplacer la couleur de fond par du blanc
-    image = remplacer_couleur(imageBase, liste)
+    imageNoirSurBlanc = remplacer_couleur(imageBase, liste, 10)
 
     # image to CV_8UC1
-    imageAma = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    contourAmas = identification_amas(imageAma)
+    imageContours = cv2.cvtColor(imageNoirSurBlanc, cv2.COLOR_BGR2GRAY)
+    # On récupère les amas de pixels noirs
+    contourAmas = identification_amas(imageContours)
     print('Nombre d\'amas de pixels noirs :', len(contourAmas))
 
-    # Afficher les amas de pixels noirs
-    cv2.drawContours(image, contourAmas, -1, (0, 255, 0), 3)
+    # Encadrer les amas de pixels noirs
+    imageContours = encadrer_amas(imageBase, contourAmas)
+    # Numéroter les amas de pixels noirs
+    imageContours = numeroter_amas(imageContours, contourAmas)
 
-    # Créer la fenêtre d'affichage de l'image en premier plan
-    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    # enregistrer l'image noir sur blanc
+    cv2.imwrite('image_noir_sur_blanc.png', imageNoirSurBlanc)
 
-    # Afficher l'image
-    cv2.imshow('image', image)
+    # Enregistrer l'image avec les amas de pixels noirs
+    cv2.imwrite('image_contours.png', imageContours)
 
-    # enregistrer l'image
-    cv2.imwrite('image.png', image)
+    # On récupère l'image de la pièce 1
+    imagePiece = piece_info(contourAmas, 0, imageBase)
+    # Enregistrer l'image de la pièce 1
+    cv2.imwrite('image_piece_1.png', imagePiece)
 
     # end time
     end = time.time()
     elapsed = round(end - start, 2)
     print('Temps d\'exécution :', elapsed, ' secondes')
-
-    # Boucle infinie pour afficher l'image
-    while True:
-        key = cv2.waitKey(30) & 0x0FF
-        if key == 27 or key == ord('q'):
-            print('arrêt du programme par l\'utilisateur')
-            break
-
-    cv2.destroyWindow('image')
-    print('Fin du programme')
